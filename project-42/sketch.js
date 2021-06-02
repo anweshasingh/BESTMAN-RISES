5eb72f7f68e45af5479a835e3d58aa701531cdcf
anweshasingh <72261344+anweshasingh@users.noreply.github.com>
2021-06-02 19:50:22 +0530

    Update sketch.js

--- project-42/sketch.js
+++ project-42/sketch.js
@@ -16,10 +16,10 @@ var drops=[];
 var thunderCreatedFrame=0;
 
 function preload(){
-    thunder1 = loadImage("thunderbolt/1.png");
-    thunder2 = loadImage("thunderbolt/2.png");
-    thunder3 = loadImage("thunderbolt/3.png");
-    thunder4 = loadImage("thunderbolt/4.png");
+    thunder1 = loadImage("images/thunderbolt/1.png"); 
+    thunder2 = loadImage("images/thunderbolt/2.png"); 
+    thunder3 = loadImage("images/thunderbolt/3.png"); thunder4 = 
+    loadImage("images/thunderbolt/4.png");
 
     batAnimation = loadAnimation("bat/bat1.png","bat/bat2.png","bat/bat3.png",
                         "bat/bat4.png","bat/bat5.png","bat/bat6.png",
